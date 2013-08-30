585482f03fa840ab3abc1100e32c585712db9e2c
aolofsson <andreas@adapteva.com>
2013-08-30 10:15:30 -0400

    Removed duplicate file, added license

--- gen0/fpga/hdl/parallella_7020_top.v
+++ gen0/fpga/hdl/parallella_7020_top.v
@@ -1,3 +1,26 @@
+/*
+  File: version.v
+ 
+  This file is part of the Parallella Project
+
+  Copyright (C) 2013 Adapteva, Inc.
+  Contributed by Roman Trogan <support@adapteva.com>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program (see the file COPYING).  If not, see
+  <http://www.gnu.org/licenses/>.
+*/
+
 module parallella_7020_top (/*AUTOARG*/
    // Outputs
    processing_system7_0_DDR_WEB_pin, GPIO12_P, GPIO12_N, GPIO13_P,
