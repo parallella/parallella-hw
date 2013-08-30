7920a63bc4192aaa90c4f0ca77a37bcadb7963d6
aolofsson <andreas@adapteva.com>
2013-08-30 09:48:29 -0400

    Removed duplicate files (can be found in comon hdl)

--- gen0/fpga/hdl/version.v
+++ gen0/fpga/hdl/version.v
@@ -1,10 +1,10 @@
 /*
-  File: mux4.v
+  File: version.v
  
-  This file is part of the Parallella FPGA Reference Design.
+  This file is part of the Parallella Project
 
   Copyright (C) 2013 Adapteva, Inc.
-  Contributed by Roman Trogan <support@adapteva.com>
+  Contributed by Andreas Olofsson <support@adapteva.com>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -20,34 +20,4 @@
   along with this program (see the file COPYING).  If not, see
   <http://www.gnu.org/licenses/>.
 */
-module mux4(/*AUTOARG*/
-   // Outputs
-   out,
-   // Inputs
-   in0, in1, in2, in3, sel0, sel1, sel2, sel3
-   );
-
-   parameter DW=99;
-  
-   //data inputs
-   input [DW-1:0]  in0;
-   input [DW-1:0]  in1;
-   input [DW-1:0]  in2;
-   input [DW-1:0]  in3;
-   
-   //select inputs
-   input           sel0;
-   input           sel1;
-   input 	   sel2;
-   input 	   sel3;
-
-   output [DW-1:0] out;
-   
-  
-   assign out[DW-1:0] = ({(DW){sel0}} & in0[DW-1:0] |
-			 {(DW){sel1}} & in1[DW-1:0] |
-			 {(DW){sel2}} & in2[DW-1:0] |
-			 {(DW){sel3}} & in3[DW-1:0]);
-   
-			
-endmodule // mux4
+`define VERSION_VALUE   32'h13080601 //Firmware version YY_MM_DD_COUNT
